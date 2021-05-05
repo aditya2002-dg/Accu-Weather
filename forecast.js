0781bf7846925c62525ee622f37178a6c8fcd525
Aditya Dhanraj Gupta <66785054+aditya2002-dg@users.noreply.github.com>
2021-05-05 07:16:23 -0700

    Add files via upload

---
+++ forecast.js
@@ -0,0 +1,17 @@
+const key='OfoSNpfWFKcr8K0xHj9AkkNEhFO4brZm';
+//get weather info
+const getWeather= async(id)=>{
+    const base='https://dataservice.accuweather.com/currentconditions/v1/';
+    const query=`${id}?apikey=${key}`;
+    const response= await fetch(base + query);
+    const data= await response.json();
+    return data[0]; 
+};
+//get city info
+const getCity= async(city)=>{
+    const base='https://dataservice.accuweather.com/locations/v1/cities/search';
+    const query=`?apikey=${key}&q=${city}`;
+    const response= await fetch(base + query);
+    const data= await response.json();
+    return data[0]; 
+};
